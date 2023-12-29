c2b243b35ec751a73b33195661744cc8ce120b7d
Gabriel Pinheiro <gabriel.pinheiro.dev@outlook.com>
2023-12-29 12:39:14 -0300

    desenvolvimento

--- script.cjs
+++ script.cjs
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
